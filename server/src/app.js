426a46195a591aaafa6ed19e0c0ba1a52d701149
abdo <masterabdotgm50@ymail.com>
2019-07-29 14:18:43 +0200

    Sockets Boilerplates

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,29 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
-io.on('connection', socket => {
+app.use(express.static('public'));
 
+io.use((socket, next) => {
+    console.log('Connection Handshake: ' + JSON.stringify(socket.handshake.query.username));
+    if(socket.handshake.query && socket.handshake.query.username) {
+        socket.cldata = {};
+        socket.cldata.username = socket.handshake.query.username;
+        next();
+    } else { next(new Error()); }
 });
 
-http.listen(3000, () => {
-    console.log('Listening');
+io.on('connection', socket => {
+    console.log("default: " + socket.cldata.username + " connected!");
+
+    socket.on("disconnect", () => {
+        console.log("default: " + socket.cldata.username + " disconnected!");
+        socket.removeAllListeners();
+    });
+});
+
+let PORT = process.env.PORT || 4213;
+http.listen(PORT, () => {
+    console.log('Listening on: ' + PORT);
 });
